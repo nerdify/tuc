49df838b86d11e534489cb39a662c389995df1e7
Hosmel Quintana <hosmelq@gmail.com>
2017-06-08 04:12:41 -0600

    add cache

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const { send } = require('micro');
-const Tuc = require('tuc');
+const { cache, createClient } = require('r-cache');
+const { getBalance } = require('tuc-promise');
 
-const tuc = new Tuc;
+const client = createClient(
+  process.env.REDIS_PORT, process.env.REDIS_HOST, { no_ready_check: true }
+);
+
+client.auth(process.env.REDIS_PASSWORD);
 
 module.exports = async (req, res) => {
   const number = req.url.replace(/.*(\d{8}).*/, '$1');
+  const cacheKey = `tuc:${number}`;
 
-  const response = await new Promise((resolve) => {
-    tuc.getBalance(number, (data) => resolve(data));
-  });
+  try {
+    const response = await cache(cacheKey, 120, () => getBalance(number));
 
-  if (tuc.isError(response)) {
-    const { code } = response.error;
+    send(res, 200, {
+      number,
+      balance: parseFloat(response),
+    });
+  } catch (err) {
+    const { code } = err;
 
     if (code === 100 || code === 104) {
       return send(res, 404);
     }
 
-    return send(res, 503);
+    send(res, 503);
   }
-
-  send(res, 200, {
-    number,
-    balance: parseFloat(response),
-  });
 }
