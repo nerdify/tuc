2e45a8a8a485f05931167b03dde0bcd9717f6fa9
Hosmel Quintana <hosmelq@gmail.com>
2017-06-24 04:11:16 -0600

    reformat code

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-const { send } = require('micro');
-const { cache, createClient } = require('r-cache');
-const { getBalance } = require('tuc-promise');
+require('dotenv').config();
+
+const {send} = require('micro');
+const {cache, createClient} = require('r-cache');
+const {getBalance} = require('tuc-promise');
 
 const client = createClient(
-  process.env.REDIS_PORT, process.env.REDIS_HOST, { no_ready_check: true }
+	// eslint-disable-next-line camelcase
+	process.env.REDIS_PORT, process.env.REDIS_HOST, {no_ready_check: true}
 );
 
 client.auth(process.env.REDIS_PASSWORD);
 
 module.exports = async (req, res) => {
-  const number = req.url.replace(/.*(\d{8}).*/, '$1');
-  const cacheKey = `tuc:${number}`;
+	const number = req.url.replace(/.*(\d{8}).*/, '$1');
+	const cacheKey = `tuc:${number}`;
 
-  try {
-    const response = await cache(cacheKey, 120, () => getBalance(number));
+	try {
+		const response = await cache(cacheKey, 120, () => getBalance(number));
 
-    send(res, 200, {
-      number,
-      balance: parseFloat(response),
-    });
-  } catch (err) {
-    const { code } = err;
+		send(res, 200, {
+			number,
+			balance: parseFloat(response)
+		});
+	} catch (err) {
+		const {code} = err;
 
-    if (code === 100 || code === 104) {
-      return send(res, 404);
-    }
+		if (code === 100 || code === 104) {
+			return send(res, 404);
+		}
 
-    send(res, 503);
-  }
-}
+		send(res, 503);
+	}
+};
